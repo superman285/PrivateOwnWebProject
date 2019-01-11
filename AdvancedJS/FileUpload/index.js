77f24b85a926a292d1db7c63ad5260eafd46c230
superman285 <superman285@126.com>
2019-01-11 13:34:42 +0800

    文件上传upload项目 sm.ms图床版+自建服务器版 图床版切记设置smfile参数 append加上 自建服务器版配好对应路由地址 搭上静态资源 update on 2019.1.10

--- AdvancedJS/FileUpload/index.js
+++ AdvancedJS/FileUpload/index.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-//const koaStaticCache = require('koa-static-cache');
+const koaStaticCache = require('koa-static-cache');
 const KoaRouter = require('koa-router');
 const multer = require('koa-multer'); //文件上传模块
 
@@ -7,11 +7,36 @@ let app = new Koa();
 let router = new KoaRouter()
 
 
+app.use(koaStaticCache({
+    //prefix: '/public',
+    dir: './',
+    dynamic: true,
+    gzip: true
+}));
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploadFiles/');
+        console.log('destinationReq',file);
+    },
+    filename: function (req, file, cb) {
+        console.log(file.originalname);
+        console.dir(file.fieldname);
+
+        let lastPointPos = file.originalname.lastIndexOf('.');
+        let ext = file.originalname.substring(lastPointPos);
+        cb(null, file.fieldname + '-' + Date.now() + ext);
+    }
+});
 
 
-router.get('/getdata',async ctx=>{
-    ctx.body = '你好啊';
-})
+const upload = multer( {
+    storage:storage
+} );
+
+router.post('/upload', upload.single('file'), async ctx => {
+    ctx.response.body = '上传成功辣';
+});
 
 
 app.use(router.routes());
