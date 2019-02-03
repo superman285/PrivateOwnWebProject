bf84728ce91eb87e124f839041ad789af31fb774
superman285 <superman285@126.com>
2019-02-03 19:44:31 +0800

    vue简历编辑器 上传github配置更新 update 2019.2.3

--- AdvancedJS/VueFamily/VueResumer/vue.config.js
+++ AdvancedJS/VueFamily/VueResumer/vue.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
 
-    assetsDir: '/PrivateOwnWebProject/AdvancedJS/VueFamily/VueResumer/dist/'
-
+    assetsDir: '/PrivateOwnWebProject/AdvancedJS/VueFamily/VueResumer/dist/',
+    publicPath: './'
 }
