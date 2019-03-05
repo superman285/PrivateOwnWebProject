a71ae9a35964d4e7c10723d735806c60a2a4d317
superman285 <superman285@126.com>
2019-03-05 21:38:03 +0800

    小更新
            2019.3.5

---
+++ AdvancedJS/MyFavorProject/SkrWalletVue/server/controller/transaction.js
@@ -0,0 +1,12 @@
+let Web3 = require('web3');
+let fs = require('fs');
+let path = require('path');
+
+var utils = require('../../utils/myUtils');
+var web3 = utils.getweb3();
+
+module.exports = {
+
+
+
+}
--- AdvancedJS/MyFavorProject/SkrWalletVue/src/views/QueryTx.vue
+++ AdvancedJS/MyFavorProject/SkrWalletVue/src/views/QueryTx.vue
@@ -1,15 +1,72 @@
 <template>
     <div>
-        querytx
+
+        <v-card>
+
+            <v-layout justify-space-between align-center>
+            <v-text-field
+                    class="tokenField"
+                    label="Token"
+                    single-line
+                    solo
+                    readonly
+                    :value="tokenType"
+            ></v-text-field>
+
+            <v-text-field
+                    class="amountField"
+                    label="金额"
+                    single-line
+                    solo
+                    @input="changeAmount($event)"
+            ></v-text-field>
+            </v-layout>
+
+            <v-text-field
+                    label="目标地址"
+                    single-line
+                    box
+            ></v-text-field>
+
+            <v-btn
+                    block
+                    color="blue lighten-2"
+                    class="white--text"
+                    @click = "sendTx"
+            >发送<v-icon right color="white">present_to_all</v-icon></v-btn>
+        </v-card>
     </div>
 </template>
 
 <script>
     export default {
-        name: "QueryTx"
+        name: "QueryTx",
+        data(){
+            return {
+                tokenType: "ETH",
+                tokenAmount: ""
+            }
+        },
+        methods: {
+            changeAmount(ev){
+                this.tokenAmount = ev.target.value;
+            },
+
+
+            sendTx(){
+
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .tokenField,.amountField {
+        flex: .49;
+    }
+    .v-card {
+        margin: 2rem auto;
+        padding: 2rem;
+        width: 60%;
+    }
 </style>
