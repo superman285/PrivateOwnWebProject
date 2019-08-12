e120e0f28d0635c512d23222f45caf5f69ab868b
superman285 <superman285@126.com>
2019-08-12 10:26:11 +0800

    update dist

--- AdvancedJS/VueFamily/VueResumer/vue.config.js
+++ AdvancedJS/VueFamily/VueResumer/vue.config.js
@@ -3,11 +3,11 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     publicPath: './',
-    configureWebpack: config=>{
+    /*configureWebpack: config=>{
         if (process.env.NODE_ENV === 'production') {
             config.mode = "production";
             config.optimization.minimize = true;
-            /*config.optimization.minimizer[0] = new TerserPlugin({
+            /!*config.optimization.minimizer[0] = new TerserPlugin({
                     terserOptions: {
                         compress: {
                             warnings: false,
@@ -15,8 +15,26 @@ module.exports = {
                             drop_console: true
                         }
                     }
-                });*/
+                });*!/
             config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
         }
+    },*/
+    configureWebpack: {
+        //mode: "production",
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            warnings: false,
+                            drop_debugger: true,
+                            drop_console: true
+                        }
+                    }
+                })
+            ]
+        },
+
     },
 };
